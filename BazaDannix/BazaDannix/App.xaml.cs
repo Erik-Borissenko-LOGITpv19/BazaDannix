86c1d81190a990b1e172db2e19e4031182ebe0fc
SnowBall <SnowBall@SNOWBALL1>
2021-01-18 12:34:54 +0200

    Добавьте файлы проекта.

---
+++ BazaDannix/BazaDannix/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using BazaDannix.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BazaDannix
+{
+    public partial class App : Application
+    {
+        
+            public App()
+            {
+                MainPage = new NavigationPage(new FriendsListPage());
+            }
+
+            protected override void OnStart()
+            { }
+
+            protected override void OnSleep()
+            { }
+
+            protected override void OnResume()
+            { }
+        
+    }
+}
---
+++ BazaDannix/BazaDannix/Views/FriendPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using BazaDannix.ViewModels;
+
+using Xamarin.Forms;
+
+namespace BazaDannix.Views
+{
+    //   [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FriendPage : ContentPage
+    {
+        public FriendViewModel ViewModel { get; private set; }
+        public FriendPage(FriendViewModel vm)
+        {
+            InitializeComponent();
+            ViewModel = vm;
+            this.BindingContext = ViewModel;
+        }
+    }
+}
